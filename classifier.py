c543159c75ee0f2abc1b34afde931b0e7824cce8
varshabina <30861926+varshabina@users.noreply.github.com>
2020-02-02 20:52:38 +0530

    neww

--- classifier.py
+++ classifier.py
@@ -1,112 +1,119 @@
-
-from sklearn import tree
-from sklearn import ensemble
-from sklearn import linear_model
-from sklearn.metrics import accuracy_score
-
-def trees( x_train, x_test, y_train, y_test ):
-
-    res = []
-    print("hello trees")
-    m = tree.DecisionTreeClassifier()
-    m.fit(x_train, y_train)
-    print("fiting")
-    predictions = m.predict(x_test)
-    acc = accuracy_score(y_test,predictions)
-
-    res.append( ( acc , "DecisionTreeClassifier" ) )
-
-    m = tree.ExtraTreeClassifier()
-    m.fit(x_train, y_train)
-    predictions = m.predict(x_test)
-    acc = accuracy_score(y_test,predictions)
-
-    res.append( ( acc , "ExtraTreeClassifier" ) )
-
-    print(res)
-
-    return res
-
-
-def ensembles( x_train, x_test, y_train, y_test ):
-
-    res = []
-    m = ensemble.AdaBoostClassifier()
-    m.fit(x_train, y_train)
-    predictions = m.predict(x_test)
-    acc = accuracy_score(y_test,predictions)
-
-    res.append( ( acc , "AdaBoostClassifier" ) )
-
-    # print(res)
-
-    m = ensemble.BaggingClassifier()
-    m.fit(x_train, y_train)
-    predictions = m.predict(x_test)
-    acc = accuracy_score(y_test,predictions)
-
-    res.append( ( acc , "BaggingClassifier" ) )
-
-
-    m = ensemble.GradientBoostingClassifier()
-    m.fit(x_train, y_train)
-    predictions = m.predict(x_test)
-    acc = accuracy_score(y_test,predictions)
-
-    res.append( ( acc , "GradientBoostingClassifier" ) )
-
-    return res
-
-
-def lines( x_train, x_test, y_train, y_test ):
-
-    res = []
-    m = linear_model.RidgeClassifier()
-    m.fit(x_train, y_train)
-    predictions = m.predict(x_test)
-    acc = accuracy_score(y_test,predictions)
-
-    res.append( ( acc , "RidgeClassifier" ) )
-
-
-    m = linear_model.SGDClassifier()
-    m.fit(x_train, y_train)
-    predictions = m.predict(x_test)
-    acc = accuracy_score(y_test,predictions)
-
-    res.append( ( acc , "SGDClassifier" ) )
-
-    return res
-
-def classify( x_train, x_test, y_train, y_test ):
-
-    result = {}
-
-    r1 = trees( x_train, x_test, y_train, y_test )
-    r2 = lines( x_train, x_test, y_train, y_test )
-    r3 = ensembles( x_train, x_test, y_train, y_test )
-
-    res = r1 + r2 + r3
-
-    res.sort(reverse=True)
-
-    print(res)
-
-    for val , name in res[:4]:
-        result[name] = val
-
-
-    return result
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
 
 
 
+def preprocess( FILE , TARGET , UNWANTED ):
+
+    FILE = 'uploads/' + FILE
+    
+
+    f = open(FILE , 'r+')
+
+    HEADER , SEP = findHeaderAndSEP(f)
+
+    f.close()
+
+    # Open File
+
+    data = pd.read_csv(FILE , sep=SEP , header=HEADER )
+
+    #Remove Unwanted
+            
+    data = data.drop(UNWANTED , axis=1)
+
+    #Remove more than half missing data
+
+    data = data.dropna(thresh=0.5,axis=1)
+
+    #Seperating X and Y
+
+    if TARGET is not None:
+        Y = data[TARGET]
+        X = data.drop([TARGET] , axis = 1)
+        data = X
+
+    # One Hot Encode String data
+
+    def encode_and_bind(original_dataframe, feature_to_encode):
+        dummies = pd.get_dummies(original_dataframe[[feature_to_encode]])
+        res = pd.concat([original_dataframe, dummies], axis=1)
+        res = res.drop([feature_to_encode] , axis=1)
+        return res
+
+    columns = data.columns
+
+    for c in columns:
+        
+        if data[c].dtype == 'object':
+            
+            data = encode_and_bind(data , c)
+
+    if TARGET is None:
+        return data
+
+    # Train-Test Split
+
+    x_train,x_test,y_train,y_test = train_test_split( data ,Y,test_size=.34)
+
+    return x_train,x_test,y_train,y_test
+
+
+
+def findHeaderAndSEP(f):
+
+    # Finds Seperator
+
+    line = f.readline().strip()
+    SEP = None
+    if ',' in line:
+        SEP = ','
+    elif ':' in line:
+        SEP = ':'
+    elif ';' in line:
+        SEP = ';'
+
+    line1 = line.split(SEP)
+    line2 = f.readline().strip().split(SEP)
+
+    if SEP is None:
+        SEP = '\s+'
+
+    types1 = []
+    types2 = []
+
+    # Finds Header
+
+    for l in line1:
+        try:
+            float(l)
+            types1.append('float')
+        except:
+            types1.append('str')
+            
+            
+    for l in line2:
+        try:
+            float(l)
+            types2.append('float')
+        except:
+            types2.append('str')
+        
+        
+    HEADER = None
+    for a , b in zip( types1 , types2 ):
+        if a != b:
+            HEADER = 0
+            break
+    
+    # print("HEADER, SEP", HEADER , SEP)
+    
+    return HEADER , SEP
 
 
 
 
 
 
-
-
-
-    return result
+# preprocess('uploads/data.txt', 'quality' , [])
